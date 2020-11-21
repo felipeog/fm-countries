ff5bdd7f2b37e3d61ab39883735576cdff23b31e
felipeog <felipeog476@gmail.com>
2020-11-21 19:30:33 -0300

    Add CountryContent component

--- src/pages/Country/index.js
+++ src/pages/Country/index.js
@@ -1,17 +1,8 @@
 import { useEffect, useState } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import {
-  Container,
-  Header,
-  Image,
-  Loader,
-  Button,
-  Icon,
-  Message,
-} from 'semantic-ui-react'
+import { Container, Button, Icon } from 'semantic-ui-react'
+import CountryContent from '../../components/CountryContent'
 import { fetchByAlphaCode, fetchByAlphaCodeArray } from '../../utils/api'
-import ErrorMessage from '../../components/ErrorMessage'
 import './index.scss'
 
 function Country() {
@@ -59,104 +50,6 @@ function Country() {
     }
   }, [country])
 
-  function renderCountry() {
-    if (loadingCountry || !country || loadingBorders || !borders)
-      return <Loader active />
-    if (errorCountry || errorBorders) return <ErrorMessage />
-    if (country?.status)
-      return (
-        <Message
-          className="error"
-          color="red"
-          icon="exclamation"
-          header="Not found"
-          content="Invalid URL"
-        />
-      )
-
-    const {
-      flag,
-      name,
-      nativeName,
-      population,
-      region,
-      subregion,
-      capital,
-      topLevelDomain,
-      currencies,
-      languages,
-    } = country
-    const topLevelDomainList = topLevelDomain?.join(', ') || '---'
-    const currenciesList =
-      currencies?.map(({ name }) => name)?.join(', ') || '---'
-    const languagesList =
-      languages?.map(({ name }) => name)?.join(', ') || '---'
-
-    return (
-      <section className="country">
-        <h1 className="hidden">Country information</h1>
-
-        <Image className="country__flag" src={flag} alt={name} fluid />
-
-        <article className="country__content">
-          <Header className="country__name" as="h1">
-            {name}
-          </Header>
-
-          <div className="country__columns">
-            <div className="country__column">
-              <p>
-                <strong>Native name:</strong> {nativeName}
-              </p>
-              <p>
-                <strong>Population:</strong> {population?.toLocaleString()}
-              </p>
-              <p>
-                <strong>Region:</strong> {region}
-              </p>
-              <p>
-                <strong>Sub region:</strong> {subregion}
-              </p>
-              <p>
-                <strong>Capital:</strong> {capital}
-              </p>
-            </div>
-
-            <div className="country__column">
-              <p>
-                <strong>Top level domain:</strong> {topLevelDomainList}
-              </p>
-              <p>
-                <strong>Currencies:</strong> {currenciesList}
-              </p>
-              <p>
-                <strong>Languages:</strong> {languagesList}
-              </p>
-            </div>
-          </div>
-
-          <div className="borders">
-            <p className="borders__title">
-              <strong>Border countries:</strong>
-            </p>
-
-            {borders?.length ? (
-              borders?.map(({ name, alpha2Code }) => (
-                <Link key={alpha2Code} to={`/country/${alpha2Code}`}>
-                  <Button className="borders__button" basic>
-                    {name}
-                  </Button>
-                </Link>
-              ))
-            ) : (
-              <p>---</p>
-            )}
-          </div>
-        </article>
-      </section>
-    )
-  }
-
   return (
     <div className="Country">
       <Container as="main">
@@ -168,7 +61,13 @@ function Country() {
           </Button>
         </section>
 
-        {renderCountry()}
+        <CountryContent
+          country={country}
+          borders={borders}
+          loading={loadingCountry || !country || loadingBorders || !borders}
+          error={errorCountry || errorBorders}
+          notFound={country?.status}
+        />
       </Container>
     </div>
   )
