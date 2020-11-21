8c3a42258e0a88c62a9f72c5c87b0ea98f8060b0
felipeog <felipeog476@gmail.com>
2020-11-21 15:23:13 -0300

    Add dark theme

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,14 +1,35 @@
-import { Container, Header } from 'semantic-ui-react'
+import { useEffect, useState } from 'react'
+import { Container, Header, Icon } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import './index.scss'
 
 function AppHeader() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+  useEffect(() => {
+    isDarkTheme
+      ? document.body.classList.add('dark')
+      : document.body.classList.remove('dark')
+  }, [isDarkTheme])
+
+  function handleThemeChange() {
+    setIsDarkTheme((isDarkTheme) => !isDarkTheme)
+  }
+
   return (
     <div className="AppHeader">
-      <Container>
+      <Container className="header">
         <Link to="/">
-          <Header>Where in the world?</Header>
+          <Header className="header__title">Where in the world?</Header>
         </Link>
+
+        <div className="toggle" onClick={handleThemeChange}>
+          <Icon
+            className="toggle__icon"
+            name={`moon${isDarkTheme ? '' : ' outline'}`}
+          />
+          <p className="toggle__text">Dark mode</p>
+        </div>
       </Container>
     </div>
   )
