7b0b5df402b24b8b35fe12ccf55ca754479959cf
felipeog <felipeog476@gmail.com>
2020-11-22 00:25:36 -0300

    Improve navigation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,84 +1,55 @@
 import { useEffect, useState, useCallback, useRef } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import { Container, Input, Dropdown } from 'semantic-ui-react'
 import CountriesGrid from '../../components/CountriesGrid'
 import { regionOptions } from '../../consts/regionOptions'
 import { fetchByRegion, fetchByTerm, fetchAll } from '../../utils/api'
+import { isValidRegion } from '../../utils/isValidRegion'
 import './index.scss'
 
 function Home() {
   const history = useHistory()
+  const location = useLocation()
   const searchRef = useRef()
+  const timeoutRef = useRef(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
   const [countries, setCountries] = useState(null)
   const [term, setTerm] = useState('')
   const [region, setRegion] = useState('')
-  const [typingTimeoutId, setTypingTimeoutId] = useState(null)
 
-  const loadByRegion = useCallback(
-    function (region) {
-      setLoading(true)
-      setCountries(null)
-
-      history.push({
-        search: !region ? '' : `?region=${region}`,
+  const loadByRegion = useCallback(function (region) {
+    fetchByRegion(region)
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((e) => {
+        console.error(`Home@fetchByRegion >>>>> ${e}`)
+        setError(true)
       })
-
-      fetchByRegion(region)
-        .then((res) => res.json())
-        .then((data) => setCountries(data))
-        .catch((e) => {
-          console.error(`Home@fetchByRegion >>>>> ${e}`)
-          setError(true)
-        })
-        .finally(() => setLoading(false))
-    },
-    [history]
-  )
-
-  const loadByTerm = useCallback(
-    function (term) {
-      setLoading(true)
-      setCountries(null)
-
-      history.push({
-        search: !term ? '' : `?term=${term}`,
+      .finally(() => {
+        setTerm('')
+        setRegion(region)
+        setLoading(false)
       })
+  }, [])
 
-      fetchByTerm(term)
-        .then((res) => res.json())
-        .then((data) => setCountries(data))
-        .catch((e) => {
-          console.error(`Home@fetchByTerm >>>>> ${e}`)
-          setError(true)
-        })
-        .finally(() => {
-          setLoading(false)
-          searchRef.current.focus()
-        })
-    },
-    [history]
-  )
+  const loadByTerm = useCallback(function (term) {
+    fetchByTerm(term)
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((e) => {
+        console.error(`Home@fetchByTerm >>>>> ${e}`)
+        setError(true)
+      })
+      .finally(() => {
+        setRegion('')
+        setTerm(term)
+        setLoading(false)
+        searchRef.current.focus()
+      })
+  }, [])
 
-  useEffect(() => {
-    setLoading(true)
-
-    const query = new URLSearchParams(window.location.search)
-    const [region, term] = [query.get('region'), query.get('term')]
-
-    if (region) {
-      setRegion(region)
-      loadByRegion(region)
-      return
-    }
-
-    if (term) {
-      setTerm(term)
-      loadByTerm(term)
-      return
-    }
-
+  const loadAll = useCallback(function () {
     fetchAll()
       .then((res) => res.json())
       .then((data) => setCountries(data))
@@ -86,26 +57,53 @@ function Home() {
         console.error(`Home@fetchAll >>>>> ${e}`)
         setError(true)
       })
-      .finally(setLoading(false))
-  }, [loadByRegion, loadByTerm])
+      .finally(() => {
+        setRegion('')
+        setTerm('')
+        setLoading(false)
+      })
+  }, [])
+
+  useEffect(() => {
+    setLoading(true)
+    setCountries(null)
+
+    const query = new URLSearchParams(location.search)
+    const [region, term] = [query.get('region'), query.get('term')]
+
+    if (region && isValidRegion(region)) {
+      loadByRegion(region)
+      return
+    }
+
+    if (term) {
+      loadByTerm(term)
+      return
+    }
+
+    loadAll()
+  }, [loadByRegion, loadByTerm, loadAll, location])
 
   function handleSearchChange(e) {
     e.preventDefault()
-    clearTimeout(typingTimeoutId)
+    clearTimeout(timeoutRef.current)
 
     const term = e.target.value
-
-    setRegion('')
     setTerm(term)
 
-    setTypingTimeoutId(() => setTimeout(() => loadByTerm(term), 600))
+    timeoutRef.current = setTimeout(() => {
+      history.push({
+        search: !term ? '' : `?term=${term}`,
+      })
+    }, 600)
   }
 
   function handleRegionChange(_, data) {
-    setRegion(data.value)
-    setTerm('')
+    const region = data.value
 
-    loadByRegion(data.value)
+    history.push({
+      search: !region ? '' : `?region=${region}`,
+    })
   }
 
   const inputOptions = {
